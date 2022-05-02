321a7ee28ab86d5bc4394d9d1effa0b7de06c874
CompSci-EHU <25032348@edgehill.ac.uk>
2022-05-02 14:15:52 +0100

    fixed added with energy history regs

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 from umodbus.serial import Serial as ModbusRTUMaster
 from umodbus.modbus import ModbusRTU
-from machine import UART
 import time
-import struct
 
+# import struct
+from machine import UART
 
+# Change this to False to disable energy history registers
+experimental = True
+
+# Used for serial link to server
 class UARTClass:
     def __init__(
         self,
-        address,
         port: int,
-        baudrate=115200,
-        parity=None,
-        stop_bits=1,
-        data_bits=8,
-        timeout=5,
-        tries=3,
+        baudrate: int,
     ):
-
         self.serline = UART(port, baudrate)
-        self.baudrate = baudrate
-        self.parity = 0
-        self.stop_bits = stop_bits
-        self.data_bits = data_bits
-        self.timeout = timeout
-        self.tries = tries
-        self.address = address
-
         self.serline.init(self.baudrate, bits=8, parity=None, stop=1)
 
     def send(self, request):
@@ -82,42 +71,103 @@ def main():
     )
 
     # READ HREGS read_holding_registers from Meter
-    watt_register = 0x96  # 00 0C, 12+1 = read 13 and 14 registor
-    watt_value = meter.read_input_registers(
-        slave_addr=meter_addr, starting_addr=watt_register, register_qty=2, signed=True
-    )
-    watt_value = watt_value * 1000
+    ## https://owen-brothers.co.uk/pub/media/wysiwyg/OB737-CT_User_Manual.pdf
+    OB737_active_power = 0x96  # <- OB737 # SDM230 0C, 12+1 = read 13 and 14 registor
+    OB737_export_active_energy = 0x0900  # (kWh)
+    OB737_import_active_energy = 0x0800  # (kWh)
 
-    print(f"Status of hreg {meter_addr}: {watt_value}")
-    # print(watt_value)
-    print(client._register_dict)
-    time.sleep(0.2)
-    while True:
-        # Read Modbus Request
-        client.process()
-        # time.sleep(0.25)
-        # Request watt from meter
-        watt_value = meter.read_input_registers(
+    # values for SDM230 sinle phase Solax meter
+    ## https://midsummerwholesale.co.uk/pdfs/Solax-meter-chint-Datasheet.pdf
+    SDM230_active_power = 0xC  # 12 + 1
+    SDM230_export_active_energy = 0x4B  # 74 + 1 in hex (kWh)
+    SDM230_import_active_energy = 0x49  # 72 + 1 in hex (kWh)
+
+    print("Performing inital test readings")
+
+    print(f"Reading grid power register {OB737_active_power}")
+    active_power = meter.read_input_registers(
+        slave_addr=meter_addr,
+        starting_addr=OB737_active_power,
+        register_qty=2,
+        signed=True,
+    )
+    active_power = active_power * 1000
+    print(f"Status of hreg {meter_addr}: {active_power}W (grid power)\n")
+
+    # new feature enabled?
+    if experimental:
+        print(f"Reading grid export register {OB737_export_active_energy}")
+        export_active_energy = meter.read_input_registers(
             slave_addr=meter_addr,
-            starting_addr=watt_register,
+            starting_addr=OB737_export_active_energy,
             register_qty=2,
             signed=True,
         )
-        watt_value = watt_value * 1000
-        # RS485 to server ascii
-        ser.send(str(watt_value) + "\n")
-        # Convert Float to Int
-        watt = int(watt_value)
-        # write Data int the Modbus register, that get read by Solax Invertor
-        reg_HREGS[14] = [watt]
+        print(f"Status of hreg {meter_addr}: {export_active_energy}kWh (grid export)\n")
 
-        print(f"Status of hreg {meter_addr}: {reg_HREGS[14]}")
+        print(f"Reading grid import register {OB737_import_active_energy}")
+        import_active_energy = meter.read_input_registers(
+            slave_addr=meter_addr,
+            starting_addr=OB737_import_active_energy,
+            register_qty=2,
+            signed=True,
+        )
+        print(f"Status of hreg {meter_addr}: {import_active_energy}kWh (grid import)\n")
+
+    print(client._register_dict)
+    time.sleep(0.2)
+    while True:
+        try:
+            # Read Modbus Request
+            client.process()
+            # time.sleep(0.25)
+            # Request watt from meter
+            active_power = meter.read_input_registers(
+                slave_addr=meter_addr,
+                starting_addr=OB737_active_power,
+                register_qty=2,
+                signed=True,
+            )
+
+            active_power = active_power * 1000
+            # RS485 to server ascii
+            ser.send(str(active_power) + "\n")
+            reg_HREGS[14] = [int(active_power)]
+            print(f"Status of hreg {meter_addr}: {reg_HREGS[14]}W (grid value)")
+
+            # new feature enabled?
+            if experimental:
+                export_active_energy = meter.read_input_registers(
+                    slave_addr=meter_addr,
+                    starting_addr=OB737_export_active_energy,
+                    register_qty=2,
+                    signed=True,
+                )
+                reg_HREGS[76] = [int(export_active_energy)]
+                print(
+                    f"Status of hreg {meter_addr}: {reg_HREGS[76]}kWh (exported value)"
+                )
+
+                import_active_energy = meter.read_input_registers(
+                    slave_addr=meter_addr,
+                    starting_addr=OB737_import_active_energy,
+                    register_qty=2,
+                    signed=True,
+                )
+                reg_HREGS[78] = [int(import_active_energy)]
+                print(
+                    f"Status of hreg {meter_addr}: {reg_HREGS[78]}kWh (imported value"
+                )
+
+        except Exception:
+            print("Lazily ignored errors")
 
         time.sleep(0.3)
+        # end of loop
 
 
 if __name__ == "__main__":
-    # port=2, address=2
-    ser = UARTClass(port=3, address=2)
+    # UART 3 -> Server serial RS485
+    ser = UARTClass(port=3, baudrate=115200)
     print("MAIN")
     main()
